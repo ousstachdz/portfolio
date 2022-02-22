04927907140935a81d0cbaaf18befb34b7a7973d
ousstachdz <oussama.tachi@outlook.com>
2022-02-22 05:04:28 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskbarComponent } from './taskbar/taskbar.component';
+import { IconComponent } from './icon/icon.component';
+import { DesktopComponent } from './desktop/desktop.component';
+import { WindowComponent } from './window/window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskbarComponent,
+    IconComponent,
+    DesktopComponent,
+    WindowComponent
   ],
   imports: [
     BrowserModule,
